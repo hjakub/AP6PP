4a76099da29a28e34e7efa8effc2ce0c8e6a355f
hjakub <j_hrabec@utb.cz>
2025-03-19 13:56:18 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'AP6PP',
+  webDir: 'www'
+};
+
+export default config;
